faa8faa1dabb07b4c5fc66acb1698629b57429f9
ahutchful <74930349+ahutchful@users.noreply.github.com>
2022-03-13 19:24:08 -0400

    Update Frontend.py

--- Frontend.py
+++ Frontend.py
@@ -44,8 +44,8 @@ if uploaded_file is not None:
        st.write('Uploading(add progress bar here)') 
        
 if st.button('Predict!'):
-    df_json = json.loads(dataframe.to_json(orient='records'))
-    prediction = requests.post('http://backend:8080/prediction/', json=df_json)
+    df_json = json.loads(string_data)
+    prediction = requests.post('http://backend:8080/prediction/', json=df_json, headers={"Content-Type":"application/json"})
     st.write(prediction) ##begin prediction and output results here
     st.write(prediction.text)
         
