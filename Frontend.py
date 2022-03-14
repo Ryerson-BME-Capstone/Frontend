f948438b6dca7732079052d90345fece31391e7d
ahutchful <74930349+ahutchful@users.noreply.github.com>
2022-03-14 11:46:08 -0400

    Update Frontend.py

--- Frontend.py
+++ Frontend.py
@@ -43,7 +43,7 @@ if uploaded_file is not None:
        
 if st.button('Predict!'):
     df_json = dataframe.to_json(orient='records')
-    prediction = requests.post('http://backend:8080/prediction/', json=json.dumps(df_json), headers={"Content-Type": "application/json"})
+    prediction = requests.post('http://backend:8080/prediction/', json=df_json, headers={"Content-Type": "application/json"})
     st.write(prediction) ##begin prediction and output results here
     st.write(prediction.text)
         
