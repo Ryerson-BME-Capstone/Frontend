fa3a22d7a7a03908f16579f4fc5080506fabfc2f
ahutchful <74930349+ahutchful@users.noreply.github.com>
2022-03-15 14:43:27 -0400

    Update Frontend.py

--- Frontend.py
+++ Frontend.py
@@ -43,11 +43,8 @@ if st.button('Predict!'):
     data = dataframe.to_dict(orient='records')
     payload = data[0]
     prediction = requests.post("http://backend:8080/prediction/", json=payload, headers={"Content-Type": "application/json"})
-    st.write(prediction.text)
-    if prediction == '1':
-        st.write('You will survive')
-    if prediction == '0':
-        st.write('You will not survive')
+    st.write(prediction.status_code)
+    st.write(prediction)
 
 
 st.header('FAQ', anchor=None)
